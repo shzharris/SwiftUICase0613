9c05cbb6c40b2f0069f8ae6ef238379ee27f3e09
Harris.Mac <shaohaizhou36@gmail.com>
2025-06-12 13:48:52 +0800

    Initial Commit

---
+++ InternetSync/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  InternetSync
+//
+//  Created by HarrisShao on 2025/6/12.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ InternetSync/InternetSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  InternetSyncApp.swift
+//  InternetSync
+//
+//  Created by HarrisShao on 2025/6/12.
+//
+
+import SwiftUI
+
+@main
+struct InternetSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
